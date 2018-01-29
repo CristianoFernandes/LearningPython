7be48426708ce6413de0529eca1dad123a2c37f7
Cristiano Fernandes <fernandescristiano@gmail.com>
2018-01-28 23:18:34 -0200

    Primeiro commmit dos arquivos do curso de Python.

---
+++ exercises/Ex_002.py
@@ -0,0 +1,8 @@
+print('########## Exercícios de Pyhton ##########')
+print('##########    Exercício  002    ##########')
+
+real_number = float(input('Digite um número real: '))
+print('A parte inteira do número {} é {}'.format(real_number, int(real_number)))
+
+print('##########################################')
+print('##########################################')
