27fad5eb4da64b8bc400e538151abace33a35952
Cristiano Fernandes <fernandescristiano@gmail.com>
2018-01-29 18:46:12 -0200

    Exercicio 4 do curso de Python.

--- exercises/Ex_002.py
+++ exercises/Ex_002.py
@@ -1,8 +1,6 @@
 print('########## Exercícios de Pyhton ##########')
 print('##########    Exercício  003    ##########')
 nome = input('Digite o seu nome: ')
-print ('É um prazer te conhecer {}'.format(nome))
+print('É um prazer te conhecer {}'.format(nome))
 print('##########################################')
 print('##########################################')
-
-
--- exercises/Ex_003.py
+++ exercises/Ex_003.py
@@ -1,6 +1,7 @@
 print('########## Exercícios de Pyhton ##########')
 print('##########    Exercício  003    ##########')
-
-
+num = int(input('Digite um número qualquer \n'))
+num2 = int(input('Digite outro nuúmero \n'))
+print('A soma entre {} e {} é {}'.format(num, num2, (num + num2)))
 print('##########################################')
 print('##########################################')
