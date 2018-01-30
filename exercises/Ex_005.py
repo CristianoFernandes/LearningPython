1941eb662ab60a029c66532ec233e5968a55d2aa
Cristiano Fernandes <fernandescristiano@gmail.com>
2018-01-30 18:17:16 -0200

    Exercicio 5 do curso de Python.

---
+++ exercises/Ex_005.py
@@ -0,0 +1,6 @@
+print('########## Exercícios de Python ##########')
+print('##########    Exercício  005    ##########')
+mainNumber = int(input('Digite um número qualquer: '))
+print('O antecessor do número {} é {} e o sucessor é {}'.format(mainNumber, mainNumber - 1, mainNumber + 1))
+print('##########################################')
+print('##########################################')
