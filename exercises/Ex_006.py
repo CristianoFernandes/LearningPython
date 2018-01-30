84f805e4f0df57bcfc3ee93432702d7b76704ac8
CristianoFernandes <fernandescristiano@gmail.com>
2018-01-30 18:24:55 -0200

    Exercicio 6 do curso de Python.

---
+++ exercises/Ex_006.py
@@ -0,0 +1,11 @@
+print('########## Exercícios de Python ##########')
+print('##########    Exercício  005    ##########')
+import math
+
+mainNumber = int(input('Digite um número qualquer: '))
+print('O dobro do número {} é {}'.format(mainNumber, (mainNumber * 2)))
+print('O triplo do número {} é {}'.format(mainNumber, (mainNumber * 3)))
+print('A metade do número {} é {}'.format(mainNumber, (mainNumber / 2)))
+print('A raiz quadrada do número {} é {}'.format(mainNumber, math.sqrt(mainNumber)))
+print('##########################################')
+print('##########################################')
