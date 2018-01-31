c8b15b86271a3fe82241472ef1014ab75d1c78fa
Cristiano Fernandes Costa <fernandescristiano@gmail.com>
2018-01-31 07:49:07 -0200

    Exercicio 8 do curso de Python.

--- exercises/Ex_007.py
+++ exercises/Ex_007.py
@@ -1,7 +1,5 @@
 print('########## Exercícios de Python ##########')
 print('##########    Exercício  007    ##########')
-import math
-
 nota1 = float(input('Digite a primeira nota do aluno: '))
 nota2 = float(input('Digite a segunda nota do aluno: '))
 print('A média de nota do aluno é {:.2f}'.format((nota1 + nota2) / 2))
