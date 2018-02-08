0bfe9b5bf924d87e9da73b8112f4c858f7c1052c
Cristiano Fernandes <fernandescristiano@gmail.com>
2018-02-08 18:48:38 -0200

    Exercicio 15 do curso de Python.

---
+++ exercises/Ex_014.py
@@ -0,0 +1,8 @@
+import math
+import
+
+numero = int(input('Digite um número qualquer: '))
+print('O dobro do número {} é {}'.format(numero, numero * 2))
+print('A raiz quadadra do número {} é {}'.format(numero, math.sqrt(numero)))
+print('O número {} elevado ao quadrado é {}'.format(numero, pow(numero, 2)))
+print('O fatorial do numero {} é {}'.format(numero, math.factorial(numero)))
