3093d871edb961ae2a94c1a2c0d56db0ee0d9a7d
Cristiano Fernandes Costa <fernandescristiano@gmail.com>
2018-02-15 00:36:30 -0200

    Desafio 21 do curso de Python.

--- exercises/Ex_020.py
+++ exercises/Ex_020.py
@@ -1,7 +1,7 @@
 import random
 
-print('#' * 42)
-print('#' * 15, 'Exercício 019', '#')
+print('#' * 45)
+print('#' * 15, 'Exercício 019', '#' * 15)
 name1 = input('Digite o nome do aluno 1: ')
 name2 = input('Digite o nome do aluno 2: ')
 name3 = input('Digite o nome do aluno 3: ')
@@ -9,3 +9,4 @@ name4 = input('Digite o nome do aluno 4: ')
 lista = [name1, name2, name3, name4]
 ordem = random.shuffle(lista)
 print('A ordem da apresentação será: {}'.format(lista))
+print('#' * 45)
--- exercises/Ex_021.py
+++ exercises/Ex_021.py
@@ -1,2 +1,15 @@
-print('#' * 42)
-print('#' * 15, 'Exercício 019', '#')
+print('#' * 45)
+print('#' * 15, 'Exercício 021', '#' * 15)
+
+from pygame import mixer
+import time
+
+mixer.init()
+
+mixer.music.load('music.mp3')
+mixer.music.play()
+end = mixer.music.get_endevent()
+
+time.sleep(240)
+
+print('#' * 45)
