97cd9c7fb0af529ee0962d3dbcd8d15249e1684f
CristianoFernandes <fernandescristiano@gmail.com>
2018-02-15 18:51:26 -0200

    Desafio 25 do curso de Python

---
+++ exercises/Ex_025.py
@@ -0,0 +1,8 @@
+print('#' * 45)
+print('#' * 15, 'Exercício 023', '#' * 15)
+
+nome = str(input('Digite seu nome completo: '))
+sobrenome = str(input('Digite o sobrenome procurado: ')).lower()
+print('Seu nome possui o sobrenome procurado? {}'.format(sobrenome in nome.lower()))
+
+print('#' * 45)
