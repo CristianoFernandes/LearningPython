fc5fb363233da25a015c0dc59809e236ca49b2d5
CristianoFernandes <fernandescristiano@gmail.com>
2018-02-15 18:02:28 -0200

    Desafio 24 do curso de Python

--- exercises/Ex_021.py
+++ exercises/Ex_021.py
@@ -2,12 +2,13 @@ print('#' * 45)
 print('#' * 15, 'Exercício 021', '#' * 15)
 
 from pygame import mixer
+
 import time
 
 mixer.init()
 mixer.music.load('music.mp3')
 print('Playing music...')
 mixer.music.play()
-time.sleep(60)
-
+time.sleep(240)
+print('Acabou a música! ')
 print('#' * 45)
--- exercises/Ex_022.py
+++ exercises/Ex_022.py
@@ -1,9 +1,12 @@
 print('#' * 45)
-print('#' * 15, 'Exercício 021', '#' * 15)
+print('#' * 15, 'Exercício 022', '#' * 15)
 
 nome = str(input('Digite o seu nome completo: ')).strip()
 print('Analizando seu nome... ')
 print('O seu nome em letras maiúsculas é: ', nome.upper())
 print('O seu nome em letras minúsculas é: ', nome.lower())
-print('O seu nome completo tem ', (len(nome) - (nome.count(' '))), 'sem contar os espaços')
-print('O seu primeiro nome tem {} letras'.format(nome.find(' ')))
+print('O seu nome completo tem ', (len(nome) - (nome.count(' '))), 'letras, sem contar os espaços')
+separa = nome.split()
+print('O seu primeiro nome é {} e tem {} letras'.format(separa[0], len(separa[0])))
+
+# print('O seu primeiro nome tem {} letras'.format(nome.find(' '))) // alternativa
